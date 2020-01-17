043885b35d9e11dcdc720a43bc4de8661a663611
Bambofy <rbamfordz@gmail.com>
2020-01-17 20:28:12 +0000

    initial code commit

---
+++ JPG_2_SERIAL/JPG_2_SERIAL/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JPG_2_SERIAL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
